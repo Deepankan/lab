cb7574cdec897e98ed4b45eb80e5c1fee5e98a83
Deepankan Chitragupt <chitragupt.deepankan@gmail.com>
2016-06-28 17:39:14 +0530

    New cIty

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -47,7 +47,7 @@ class CitiesController < ApplicationController
 
     respond_to do |format|
       if @city.save
-        format.html { redirect_to @city, notice: 'City was successfully created.' }
+        format.html { redirect_to cities_path, notice: 'City was successfully created.' }
         format.json { render :show, status: :created, location: @city }
       else
         format.html { render :new }
